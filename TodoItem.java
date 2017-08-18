738a1b62046c40fe6fe7219d5772d5a4a0addbb6
Val1706 <potapovvalentine@gmail.com>
2017-08-18 13:39:43 +0200

    Update TodoItem.java

--- TodoItem.java
+++ TodoItem.java
@@ -2,6 +2,23 @@
  * Class representing a single todo item.
  * 
  */
-public class TodoItem{
 
+public class TodoItem {
+
+    private String name;
+    Boolean is_completed = false;
+
+    public TodoItem(String name){
+        this.name = name;
+
+        }
+    public void get_completed(TodoItem this){
+        this.is_completed = true;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    
 }
